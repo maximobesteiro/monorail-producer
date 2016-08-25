23f5004e8f9df099f664b4a9b881ae73ab6b03fd
Máximo Besteiro <mbesteiro@despegar.com>
2016-08-25 11:44:34 -0300

    Added topic as command line argument

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,37 +7,39 @@ object Main {
     if(!validateArgs(args)) printUsageAndExit
 
     val env: Environment.EnumVal = Environment.fromName(args(0)).get
-    val from: Int = args(1).toInt
-    val amount: Int = args(2).toInt
-    val prefix: String = args(3)
+    val topic: String = args(1)
+    val from: Int = args(2).toInt
+    val amount: Int = args(3).toInt
+    val prefix: String = args(4)
 
-    sendRequests(from, amount, prefix, env)
+    sendRequests(topic, from, amount, prefix, env)
     System.exit(0)
   }
 
   def validateArgs(args: Array[String]): Boolean = {
-    val isValid = args.length == 4 && args(2).toInt > 0
+    val isValid = args.length == 5 && args(3).toInt > 0
     isValid && Environment.fromName(args(0)).isDefined
   }
 
   def printUsageAndExit = {
     println(
       """
-        |Usage: monorail-producer [prod | rc] START AMOUNT PREFIX
+        |Usage: monorail-producer [prod | rc] TOPIC START AMOUNT PREFIX
         |
+        | TOPIC: The topic this event is referring to
         | START: Starting event index
         | AMOUNT: Amount of events
         | PREFIX: Event prefix
         |
-        |Example: monorail-producer rc 0 500 myEvent
+        |Example: monorail-producer rc seq-nums 0 500 myEvent
       """.stripMargin)
     System.exit(0)
   }
 
-  def sendRequests(from: Int, amount: Int, prefix: String, env: Environment.EnumVal) = {
-    println(s"Starting to send $amount events, starting from $from, with prefix '$prefix'\n")
+  def sendRequests(topic: String, from: Int, amount: Int, prefix: String, env: Environment.EnumVal) = {
+    println(s"Starting to send $amount events, starting from $from, with prefix '$prefix' to topic '$topic'\n")
 
-    val connector = new MonorailConnector(env)
+    val connector = new MonorailConnector(env, topic)
 
     (from until from + amount).map { case each =>
       Await.ready(connector.issuePostRequest(DummyEvent(each, prefix)).map(fr => treatResponse(fr)), 10 seconds)
